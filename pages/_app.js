90582683aa2320c9b0134633af919b1a2d7d0f5b
Wade Austin <wade@few.io>
2022-06-09 16:50:54 -0500

    connect ga

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,25 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as ga from '../lib/ga';
 
 import Layout from '../components/Layout';
 import '../styles/globals.css'
 
+
+
 function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouterChange = (url) => {
+      ga.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouterChange);
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouterChange);
+    }
+  }, [router.events])
   return( 
     <Layout>
       <Component {...pageProps} />
